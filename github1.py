82679f9dbb7bc91f0f70e08109cd789dd931d218
oybegim <sultonovaoybegim@main.com>
2022-11-03 20:20:41 +0300

    GitHub

---
+++ github1.py
@@ -0,0 +1,9 @@
+print("YANGI PROYEKTNI  GITHUBGA JOYLASH!")
+
+print("1.git init")
+print("2.git add .")
+print("3.git status")
+print("4.git commit -m ""(-//-)")
+print("5.git renote add")
+print("6.git branch -m (--//--)")
+print("7.git push -u origin (--//--)")
---
+++ github2.py
@@ -0,0 +1,7 @@
+print("O'ZGARTIRISH KIRITILGAN FAYLLARNI GITHUBGA QAYTA YUKLASH!")
+
+
+print("1.git add .")
+print("2.git status")
+print("3.git commit -m (---//---)")
+print("4.git push")
